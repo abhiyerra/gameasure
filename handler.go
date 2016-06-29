4a65d2c59a8ae9150fa42baaa8f5dcf163bdb7a6
Abhi Yerra <abhi@berkeley.edu>
2016-06-29 19:03:56 +0000

    Added a net/http middleware.

---
+++ handler.go
@@ -0,0 +1,80 @@
+package gameasure
+
+import (
+	"net/http"
+
+	"github.com/satori/go.uuid"
+)
+
+func gaClientID(r *http.Request) string {
+	gacookie, err := r.Cookie("_ga")
+	if err != nil {
+		return ""
+	}
+
+	if ga := strings.Split(gacookie.Value, "."); len(ga) == 4 {
+		return strings.Join(ga[2:], ".")
+	}
+
+	return ""
+}
+
+// GAHandler is a middleware which logs backend requests to Google
+// Analytics.
+//
+// trackerIds := map[string]string{
+// 	"api.acksin.com":  "UA-XXXXXXX-1",
+// 	"default":         "UA-XXXXXXX-2",
+// }
+// gameasure.NewGAHandler(r, trackerIds)
+type GAHandler struct {
+	TrackerIDs map[string]string
+	handler    http.Handler
+}
+
+// pageview records a pageview.
+func (s *GAHandler) pageview(clientID, host, path string) {
+	gaID, ok := s.TrackerIDs[host]
+	if !ok {
+		gaID = s.TrackerIDs["default"]
+	}
+
+	if gaId != "" {
+		log.Println("No TrackingID for Host:", host)
+		return
+	}
+
+	if clientID == "" {
+		clientId = uuid.NewV4()
+	}
+
+	New(gaID, clientID).Pageview(Pageview{
+		DocumentHost: host,
+		Page:         path,
+		Title:        path,
+	})
+}
+
+// ServeHTTP shows the requested page but logs the page request in
+// goroutine to Google Analytics.
+func (s *GAHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "OPTIONS" {
+		go s.pageview(gaClientID(r), r.Host, r.RequestURI)
+	}
+
+	s.handler.ServeHTTP(w, r)
+}
+
+// NewGAHandler creates a new GAHandler. Pass in a map of the Host to
+// TrackingID. Pass a default key for a default trackingid
+//
+// trackerIds := map[string]string{
+// 	"api.acksin.com":  "UA-XXXXXXX-1",
+// 	"default":         "UA-XXXXXXX-2",
+// }
+func NewGAHandler(handler http.Handler, trackerIds map[string]string) *GAHandler {
+	return &GAHandler{
+		handler:    handler,
+		TrackerIDs: trackerIds,
+	}
+}
